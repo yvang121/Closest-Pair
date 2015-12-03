a950deef7bc15778472f2a97f267cb71893262fa
Ye Vang <yvang@macalester.edu>
2015-12-02 21:51:00 -0600

    More testing

--- test/ClosestPairTest.java
+++ test/ClosestPairTest.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static junit.framework.TestCase.assertEquals;
-
 /**
  * Created by Ye on 12/1/2015.
  */
@@ -16,9 +14,15 @@ public class ClosestPairTest {
         GPoint pt1 = new GPoint(100, 300);
         GPoint pt2 = new GPoint(50, 150);
         GPoint pt3 = new GPoint(75, 90);
+        GPoint pt4 = new GPoint(30, 40);
+        GPoint pt5 = new GPoint(80, 120);
+        GPoint pt6 = new GPoint(75, 90);
         coord.add(pt1);
         coord.add(pt2);
         coord.add(pt3);
+        coord.add(pt4);
+        coord.add(pt5);
+        coord.add(pt6);
 
         ClosestPair closestPair = new ClosestPair(coord);
         System.out.println("Pre-sort: " + coord);
@@ -26,7 +30,7 @@ public class ClosestPairTest {
         System.out.println("Sorted by y-coordinate: " + closestPair.sortByY(coord));
         Triplet<GPoint, GPoint, Double> triplet = closestPair
                 .findClosestDist(closestPair.getSortedByXCoords(), closestPair.getSortedByYCoords());
-        assertEquals(65.0, triplet.getValue2());
-        System.out.println(triplet.getValue0().toString() +","+ triplet.getValue1().toString());
+//        assertEquals(65.0, triplet.getValue2());
+        System.out.println(triplet.getValue0() +","+ triplet.getValue1().toString() + ", " + triplet.getValue2());
     }
 }
