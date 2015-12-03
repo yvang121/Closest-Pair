adb3797db0da3c9d23f8f092e468aef805b31819
Ye Vang <yvang@macalester.edu>
2015-12-02 22:36:42 -0600

    Added clicking to add circles and an escape key listener.

--- src/ClosestPairUI.java
+++ src/ClosestPairUI.java
@@ -1,27 +1,45 @@
+import acm.graphics.GOval;
 import acm.graphics.GPoint;
 import acm.program.GraphicsProgram;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 /**
  * Created by Ye on 12/2/2015.
  */
 public class ClosestPairUI extends GraphicsProgram {
-    ArrayList<GPoint> coordinates;
+    private ArrayList<GPoint> coordinates = new ArrayList<GPoint>();
+    private static final int CIRCLE_RADIUS = 15;
 
     public void init() {
         addMouseListeners();
+        addKeyListeners();
     }
 
     public void run() {
-        coordinates = new ArrayList<GPoint>();
         while (true) {
             if (coordinates.size() > 1 ) {
                 ClosestPair closestPair = new ClosestPair(coordinates);
-//                ArrayList<Double> sortedX = closestPair.getSortedXCoords();
-//                ArrayList<Double> sortedY = closestPair.getSortedYCoords();
-//                closestPair.findClosestPair(sortedX, sortedY);
+                System.out.println(closestPair.
+                        findClosestDist(closestPair.getSortedByXCoords(), closestPair.getSortedByYCoords())
+                .getValue2());
             }
         }
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            pause(250);
+            System.exit(0);
+        }
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        GPoint point = new GPoint(e.getX(), e.getY());
+        coordinates.add(point);
+        GOval circle = new GOval(CIRCLE_RADIUS, CIRCLE_RADIUS);
+        add(circle, e.getX(), e.getY());
     }
 }
